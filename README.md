4506d4ca61ba1a0f463b527cba9c1e64d80f8c4c
Gareth Lee <lkl.g@hotmail.com>
2022-06-19 23:32:50 +1000

    Initial layout in HTML and CSS

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Tic-Tac-Toe
+
 This version of tic-tac-toe was made using:
 1. HTML
 1. CSS
 1. JavaScript
 
-## Functions
+
+
+## Gameplay
 
 
 
---
+++ js/script.js
@@ -0,0 +1,13 @@
+// What technologies will it use?
+// HTML
+// CSS
+// JavaScript
+
+// What are the base features in tic-tac-toe?
+// Board. 3x3 grid. Maybe 4x4? 5x5? How many adjustments can you make?
+// 2 players -> Either PvP or PvAI (AI difficulty modes?)
+// 2 avatars. X vs O. (Customisable?)
+// Live score. Who has won how many rounds? 
+// 
+
+
